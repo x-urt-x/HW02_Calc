79715d981108a7ce14000f6e4f3b0f38054b6f2c
urt <urt5002urt@gmail.com>
2023-01-05 21:44:57 +0200

    add logic for Calc function

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace test002
+namespace HW01_Calc1
 {
 
     class Program
@@ -21,11 +21,57 @@ namespace test002
 
         static double Calc(List<string> res)
         {
-            return 0;
+            double num1, num2;
+            if (res.Count == 1)
+            {
+                if (double.TryParse(res[0].ToString(), out num1))
+                {
+                    return num1;
+                }
+                else
+                {
+                    if (res[0][0] == '-' && res[0][1] == '(')
+                    {
+                        return -Calc(SplitByOp(res[0].ToString().Substring(1, res[0].ToString().Length - 1)));
+                    }
+                    if (res[0][0] == '(')
+                    {
+                        return Calc(SplitByOp(res[0].ToString().Substring(1, res[0].ToString().Length - 2)));
+                    }
+                    else
+                    {
+                        Console.WriteLine("InvalidInput");
+                    }
+                }
+            }
+            if (res.Count == 3)
+            {
+                if (!(double.TryParse(res[0].ToString(), out num1))) num1 = Calc(SplitByOp(res[0].ToString()));
+                if (!(double.TryParse(res[2].ToString(), out num2))) num2 = Calc(SplitByOp(res[2].ToString()));
+                return DoOp(num1, num2, res[1][0]);
+            }
+            int indMaxPrior = 1, MaxPrior = 0;
+            for (int i = 1; i < res.Count; i += 2)
+            {
+                if (Prior[res[i][0]] >= MaxPrior)
+                {
+                    indMaxPrior = i;
+                    MaxPrior = Prior[res[i][0]];
+                }
+            }
+            double n1 = Calc(res.GetRange(0, indMaxPrior));
+            double n2 = Calc(res.GetRange(indMaxPrior + 1, res.Count() - indMaxPrior - 1));
+            return DoOp(n1, n2, res[indMaxPrior][0]);
+ 
+        }
+
+        static double DoOp(double num1, double num2, char op)
+        {
+            return 1;
         }
 
 
-        static List<string> SplitByOp(string str)
+            static List<string> SplitByOp(string str)
         {
             List<string> res = new List<string> { };
             int j = 0, brCounter = str[0] == '(' ? 1 : 0;
@@ -68,12 +114,7 @@ namespace test002
             //string input = "(-(12,23/45+221-44/4)*(2*(21-8))+(56/8*-7/9))-98/(45*-3)";
             input = input.Replace(" ", "").Replace(".", ",");
 
-            //Console.WriteLine(Calc(SplitByOp(input)));
-            List<string> res = SplitByOp(input);
-            foreach (var item in res)
-            {
-                Console.WriteLine(item);
-            }
+            Console.WriteLine(Calc(SplitByOp(input)));
 
         }
     }
